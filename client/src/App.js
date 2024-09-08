eca7b9e0e3b17fd2475bb75dd7efe009c1433311
ALonx7u <203051@uptapachula.edu.mx>
2024-09-07 18:07:12 -0600

    cambios de dev

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ const App = () => {
         <h2>Welcome to React</h2>
       </div>
       <p className="App-intro">
-        HOLA <code>src/HOLI</code> PRUEBA
+        HOLA <code>src/HOLI</code> and save to reload.
       </p>
     </div>
   );
