aba4a80f8265fd3341e250b67425b60ff88b6fea
ALonx7u <203051@uptapachula.edu.mx>
2024-09-24 10:53:07 -0600

    lugar y hora

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,19 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import React, { useEffect, useRef, useState } from 'react';
-import { Button, Card, Modal } from 'react-bootstrap';
-import AudioPlayer from 'react-h5-audio-player';
-import 'react-h5-audio-player/lib/styles.css';
-import './App.css';
-import img2 from './img/img.jpg';
-import inicio from './img/inicio.jpeg';
+import "bootstrap/dist/css/bootstrap.min.css";
+import React, { useEffect, useRef, useState } from "react";
+import { Button, Card, Modal } from "react-bootstrap";
+import AudioPlayer from "react-h5-audio-player";
+import "react-h5-audio-player/lib/styles.css";
+import "./App.css";
+import i1 from "./img/1.jpeg";
+import i2 from "./img/2.jpeg";
+import i3 from "./img/3.jpeg";
+import i4 from "./img/4.jpeg";
+import i5 from "./img/5.jpeg";
+import i6 from "./img/6.jpeg";
+import img2 from "./img/img.jpg";
+import inicio from "./img/inicio.jpeg";
+import salon from "./img/salon1.jpg";
+import vestir from "./img/vestir.jpeg";
 
 // Musica
 const MusicPlayer = ({ playMusic }) => {
@@ -13,7 +21,9 @@ const MusicPlayer = ({ playMusic }) => {
 
   useEffect(() => {
     if (playMusic && audioRef.current) {
-      audioRef.current.audio.current.play();  // Reproduce la música automáticamente
+      audioRef.current.audio.current.play(); // Reproduce la música automáticamente
+    } else if (!playMusic && audioRef.current) {
+      audioRef.current.audio.current.pause(); // Pausa la música si no debe reproducirse
     }
   }, [playMusic]);
 
@@ -21,9 +31,10 @@ const MusicPlayer = ({ playMusic }) => {
     <div>
       <h3>¡Escucha nuestra canción!</h3>
       <AudioPlayer
-        ref={audioRef} 
+        ref={audioRef}
         src="/music/love.mp3"
-        onPlay={e => console.log("Reproduciendo...")}
+        onPlay={(e) => console.log("Reproduciendo...")}
+        onPause={(e) => console.log("Pausado...")}
       />
     </div>
   );
@@ -34,39 +45,51 @@ function MyModal({ onIngresar }) {
   const [show, setShow] = useState(false);
 
   useEffect(() => {
-    setShow(true);
+    setShow(true); // Muestra el modal al cargar
   }, []);
 
   const handleClose = () => {
-    setShow(false);
-    onIngresar(); // Dispara la música cuando se cierre el modal
+    setShow(false); // Cierra el modal solo al presionar el botón
+    onIngresar(); // Activa la música después de cerrar el modal
   };
 
   return (
-    <Modal show={show} onHide={handleClose} centered>
-  <Modal.Header className="d-flex flex-column align-items-center">
-    <Modal.Title className="custom-title">Bienvenidos a la invitación de Elena y Oswaldo</Modal.Title>
-    <Button variant="secondary" onClick={handleClose} className="mt-3">
-      Ingresar
-    </Button>
-  </Modal.Header>
-</Modal>
-
-
+    <Modal
+      show={show}
+      onHide={handleClose}
+      centered
+      backdrop="static" // Evita cerrar el modal haciendo clic fuera de él
+      keyboard={false} // Evita cerrar el modal con la tecla "Esc"
+    >
+      <Modal.Header className="d-flex flex-column align-items-center">
+        <Modal.Title className="custom-title">
+          Bienvenidos a la invitación de Elena y Oswaldo
+        </Modal.Title>
+        <Button
+          variant="btn btn-outline-secondary"
+          onClick={handleClose}
+          className="mt-3"
+        >
+          Ingresar
+        </Button>
+      </Modal.Header>
+    </Modal>
   );
 }
 
 // App principal
 function App() {
-  const weddingDate = new Date('2024-11-16T20:00:00');
-  const [playMusic, setPlayMusic] = useState(false);
+  const weddingDate = new Date("2024-11-16T20:00:00");
+  const [playMusic, setPlayMusic] = useState(false); // El estado inicial es `false`
 
   const calculateTimeRemaining = () => {
     const now = new Date();
     const timeDiff = weddingDate - now;
 
     const days = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((timeDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const hours = Math.floor(
+      (timeDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
     const minutes = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((timeDiff % (1000 * 60)) / 1000);
 
@@ -86,14 +109,26 @@ function App() {
   return (
     <div className="container">
       <h1>
-        <i className='far fa-heart'></i>Nuestra Boda<i className='far fa-heart'></i>
+        <i className="far fa-heart"></i>Nuestra Boda
+        <i className="far fa-heart"></i>
       </h1>
       <h2>Elena y Oswaldo</h2>
       {/* Tarjeta con la imagen y el contador */}
-      <Card className="text-center" style={{ width: '100%', maxWidth: '900px', margin: '20px auto', borderRadius: '10px', position: 'relative' }}>
+      <Card
+        className="text-center"
+        style={{
+          width: "100%",
+          maxWidth: "900px",
+          margin: "20px auto",
+          borderRadius: "10px",
+          position: "relative",
+        }}
+      >
         <Card.Img variant="top" src={inicio} alt="Elena y Oswaldo" />
         <div className="overlay">
-          <Card.Title className="wedding-title">¡Cuenta Regresiva!<i className="fab fa-gratipay"></i></Card.Title>
+          <Card.Title className="wedding-title">
+            ¡Cuenta Regresiva!<i className="fab fa-gratipay"></i>
+          </Card.Title>
           <div className="countdown-timer">
             <div className="time-section">
               <div className="time">{timeRemaining.days}</div>
@@ -114,38 +149,145 @@ function App() {
           </div>
         </div>
       </Card>
-
       {/* Reproductor de música */}
       <MusicPlayer playMusic={playMusic} />
-
-      <Card className="text-center custom-card" style={{ width: '100%', maxWidth: '900px', margin: '20px auto', borderRadius: '10px' }}>
-  <Card.Body>
-    <Card.Title className="custom-card-title">Tenemos el gusto de invitarte en este día tan importante de nuestras vidas</Card.Title>
-    <Card.Text className="custom-card-text">
-      ¡Los esperamos! Será un día inolvidable lleno de amor y felicidad.
-    </Card.Text>
-  </Card.Body>
-</Card>
-
-{/* 
-<Card className="text-center" style={{ width: '100%', maxWidth: '900px', margin: '20px auto', borderRadius: '10px', position: 'relative' }}>
-        <Card.Img variant="top" src={img1} alt="Elena y Oswaldo" />
-      </Card> */}
-
-      <Card className="text-center" style={{ width: '100%', maxWidth: '900px', margin: '20px auto', borderRadius: '10px', position: 'relative' }}>
+      <Card
+        className="text-center custom-card"
+        style={{
+          width: "100%",
+          maxWidth: "900px",
+          margin: "20px auto",
+          borderRadius: "10px",
+        }}
+      >
+        <Card.Body>
+          <Card.Title className="custom-card-title">
+            Tenemos el gusto de invitarte en este día tan importante de nuestras
+            vidas
+          </Card.Title>
+          <Card.Text className="custom-card-text">
+            ¡Los esperamos! Será un día inolvidable lleno de amor y felicidad.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <Card
+        className="text-center"
+        style={{
+          width: "100%",
+          maxWidth: "900px",
+          margin: "20px auto",
+          borderRadius: "10px",
+          position: "relative",
+        }}
+      >
         <Card.Img variant="top" src={img2} alt="Elena y Oswaldo" />
       </Card>
-
-      <h4 className="text-center">Dónde & Cuándo</h4>
-      
-
+      <h4 className="text-center">Cuándo & Dónde</h4>
+      <Card
+        className="text-center custom-card"
+        style={{
+          width: "100%",
+          maxWidth: "900px",
+          margin: "20px auto",
+          borderRadius: "10px",
+        }}
+      >
+        <Card.Body>
+          <Card.Title className="custom-card-title">Lugar</Card.Title>
+          <Card.Img
+            variant="top"
+            src={salon}
+            alt="salon"
+            style={{ maxWidth: "450px" }}
+          />
+          <Card.Text className="custom-card-text">
+            <strong>Salón:</strong> Club Country Tapachula
+            <br />
+            <strong>Dirección:</strong> Calle 3a. Ote. 33, Los Naranjos, Centro,
+            30830 Tapachula de Córdova y Ordóñez, Chis.
+          </Card.Text>
+          <Button
+            variant="info"
+            style={{ marginBottom: "20px" }}
+            href="https://maps.app.goo.gl/ypiEgAGMJitzgkrK6"
+            target="_blank"
+          >
+            Ver Mapa <i className="fas fa-map-marker-alt"></i>
+          </Button>
+          <Card.Title className="custom-card-title">Fecha y Hora</Card.Title>
+          <Card.Text className="custom-card-text">
+            <strong>Fecha:</strong> 16 de Noviembre de 2024
+            <br />
+            <strong>Hora:</strong> 20:00 hrs
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <h4 className="text-center">Código de vestimenta</h4>
+      <Card.Img
+        variant="top"
+        src={vestir}
+        alt="vestir"
+        style={{ maxWidth: "450px", margin: "20px auto" }}
+      />
+      <h4 className="text-center">Nosotros</h4>
+      <div className="row">
+        <div className="col-md-6">
+          <img
+            src={i1}
+            alt="foto1"
+            className="img-fluid"
+            style={{ borderRadius: "10px", marginBottom: "15px" }}
+          />
+        </div>
+        <div className="col-md-6">
+          <img
+            src={i2}
+            alt="foto2"
+            className="img-fluid"
+            style={{ borderRadius: "10px", marginBottom: "15px" }}
+          />
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-md-6">
+          <img
+            src={i3}
+            alt="foto3"
+            className="img-fluid"
+            style={{ borderRadius: "10px", marginBottom: "15px" }}
+          />
+        </div>
+        <div className="col-md-6">
+          <img
+            src={i4}
+            alt="foto4"
+            className="img-fluid"
+            style={{ borderRadius: "10px", marginBottom: "15px" }}
+          />
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-md-6">
+          <img
+            src={i5}
+            alt="foto5"
+            className="img-fluid"
+            style={{ borderRadius: "10px", marginBottom: "15px" }}
+          />
+        </div>
+        <div className="col-md-6">
+          <img
+            src={i6}
+            alt="foto6"
+            className="img-fluid"
+            style={{ borderRadius: "10px", marginBottom: "15px" }}
+          />
+        </div>
+      </div>
       {/* Modal */}
-      <MyModal onIngresar={() => setPlayMusic(true)} />
-
-
+      <MyModal onIngresar={() => setPlayMusic(true)} />{" "}
+      {/* Solo reproduce música al cerrar el modal */}
     </div>
-
-    
   );
 }
 
